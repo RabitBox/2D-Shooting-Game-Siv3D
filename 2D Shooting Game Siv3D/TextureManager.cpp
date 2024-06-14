ccd431552160820b2d67be34a44fc98dedd5f7f8
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2024-06-14 18:37:12 +0900

    [update] テクスチャを管理するクラスを作成
    絵文字、ファイルパス両方に対応

---
+++ 2D Shooting Game Siv3D/TextureManager.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "TextureManager.h"
+#include "defines.h"
+
+TextureManager::TextureManager() {
+
+}
+
+TextureManager::~TextureManager() {
+	_textures.clear();
+}
+
+bool TextureManager::TryGetTexture(const PathString& filePath, std::weak_ptr<Texture> outPtr) {
+	auto it = _textures.find(filePath);
+	if (it != _textures.end()) {
+		outPtr = it->second;
+		return true;
+	}
+	return false;
+}
+
+void TextureManager::LoadTexture(const PathString& filePath) {
+	auto it = _textures.find(filePath);
+	if (it != _textures.end()) {
+		return;
+	}
+
+	_textures[filePath] = std::make_shared<Texture>(Texture{ filePath });
+}
+
+void TextureManager::UnloadTexture(const PathString& filePath) {
+	auto it = _textures.find(filePath);
+	if (it != _textures.end()) {
+		_textures.erase(it);
+	}
+}
---
+++ 2D Shooting Game Siv3D/TextureManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+/// @brief パスで使用する文字列のエイリアス
+using PathString = std::u32string;
+
+/// @brief テクスチャのロード・アンロード・保持などの全般
+class TextureManager final
+{
+private:
+	// テクスチャキャッシュ
+	std::unordered_map<std::u32string, std::shared_ptr<Texture>> _textures;
+
+public:
+	TextureManager();
+	~TextureManager() {}
+
+	/// @brief ロード済のテクスチャを取得
+	///			純粋なGET関数はつくらない
+	/// @param filePath ファイルパス
+	/// @param outPtr テクスチャへのweak ptr
+	/// @return テクスチャがロード済だったか否か
+	bool TryGetTexture(const PathString& filePath, std::weak_ptr<Texture> outPtr);
+
+	/// @brief テクスチャのロード
+	/// @param filePath テクスチャのファイルパス
+	void LoadTexture(const PathString& filePath);
+
+	/// @brief テクスチャをアンロード
+	/// @param filePath テクスチャのファイルパス
+	void UnloadTexture(const PathString& filePath);
+};
+
