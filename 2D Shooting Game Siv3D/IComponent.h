e10b3329b68a80321875337ccfc1205af03e3980
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2024-06-17 01:06:23 +0900

    [update] コンポーネント基底クラスを作成
    このクラスを継承したものをコンポーネントとして扱う。デフォルトコンストラクタは認めない

---
+++ 2D Shooting Game Siv3D/IComponent.h
@@ -0,0 +1,26 @@
+﻿/**
+* コンポーネントの基本クラス
+* このクラスを継承して作られたクラスをコンポーネントとして扱う。
+*/
+#pragma once
+
+class IComponent
+{
+public:
+	// 前方宣言
+	class IObject;
+
+protected:
+	/// @brief 所有者
+	IObject* _owner;
+
+public:
+	IComponent() = delete;
+	IComponent(IObject* owner) : _owner(owner) {}
+	virtual ~IComponent() = default;
+
+public:
+	/// @brief 更新処理
+	virtual void update() {}
+};
+
--- 2D Shooting Game Siv3D/IObject.h
+++ 2D Shooting Game Siv3D/IObject.h
@@ -1,24 +1,50 @@
-﻿#pragma once
+﻿/**
+* ゲームオブジェクトの基本クラス
+* コンポーネントを追加することで、機能を増やしていく。
+*/
+#pragma once
 
 /// @brief 基本オブジェクトインターフェース
 class IObject
 {
+public:
+	class IComponent;
+
 protected:
+	/// @brief 状態
+	bool _isActive;
+
 	/// @brief 座標
 	Vec2 _position;
 
 	/// @brief 角度
 	double _angle;
 
+	/// @brief 所有コンポーネント
+	std::vector<std::unique_ptr<IComponent>> _componentList;
+
 public:
 	IObject() = default;
-	virtual ~IObject() {}
+	virtual ~IObject() = default;
 
 public:
 	/// @brief 更新
-	virtual void Update() = 0;
+	virtual void onUpdate() = 0;
 
 	/// @brief 描画
-	virtual void Draw() = 0;
+	virtual void onDraw() = 0;
+
+public:
+	/// @brief コンポーネント追加
+	/// @tparam T 追加したいコンポーネント
+	/// @return 追加したコンポーネント
+	template<class T>
+	T* addComponent();
+
+	/// @brief コンポーネント取得
+	/// @tparam T 取得したいコンポーネント
+	/// @return 取得したコンポーネント
+	template<class T>
+	T* getComponent();
 };
 
