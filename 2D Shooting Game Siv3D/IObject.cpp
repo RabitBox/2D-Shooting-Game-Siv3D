756c54f2a9375af14c0480c709548933891014c6
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2024-06-17 01:07:22 +0900

    [update] オブジェクトクラスの設計を見直し
    コンポーネント関連の処理を追加

--- 2D Shooting Game Siv3D/IObject.cpp
+++ 2D Shooting Game Siv3D/IObject.cpp
@@ -1,2 +1,20 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "IObject.h"
+
+template<class T>
+T* IObject::addComponent() {
+	_componentList.emplace_back( std::make_unique<T>( this ) );
+	auto ptr = _componentList.back();
+	return ptr.get();
+}
+
+template<class T>
+T* IObject::getComponent() {
+	for ( auto component : _componentList ) {
+		auto ptr = dynamic_cast<T*>( component.get() );
+		if ( ptr != nullptr ) {
+			return ptr;
+		}
+	}
+	return nullptr;
+}
